f94ede55078633484876efc74cae46548fc49e66
Abhishek <abhirai2006a@gmail.com>
2026-05-17 20:40:08 +0530

    May 17

--- C/coprime.c
+++ C/coprime.c
@@ -1,6 +1,35 @@
 #include<stdio.h>
 
+int isCoprime(int a , int b){
+    int t;
+    if(a<b){
+        t =a; 
+        a = b;
+        b =t;
+    }
+    while(b!=0){
+        t = a%b;
+        a = b;
+        b = t;
+    }
+    if (a==1) return 1;
+    else return 0; 
+}
+
 int main(){
-    int arr[] = {2,7,5,19,53,2};
+    int prev, curr;
+    int i,n;
+    int count =0;
+    scanf("%d", &n);
+    scanf("%d", &prev);
+    for(i=1; i<n; i++){
+        scanf("%d", &curr);
+        if(isCoprime(prev, curr)){
+            count++;
+        }
+        prev = curr;
+    }
+    printf("%d", count);
+    return 0;
     
 }
